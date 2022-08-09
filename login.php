84561d9c7b1f11181e5110186fc634ba6571b9fd
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-09 17:50:01 +0300

    6 cart.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/6
    added html

--- login.php
+++ login.php
@@ -1,1 +1,22 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="stylesheets/index.css">
+</head>
+<body>
+<div class="loginBody">
+    <form class="login" action="login.php" method="post">
+        Login
+        <input type="text" placeholder="Username" class="loginInputs">
+        <input type="text" placeholder="Password" class="loginInputs">
+        <input type="submit" class="loginInputs" value="Login">
+    </form>
+</div>
+</body>
+</html>
