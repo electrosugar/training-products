7206d41ee46b8b6da5f365332cf7ef5c267b1dce
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-09 17:24:23 +0300

    4 index.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/4
    separated the view from code

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <?php
     require_once "commons.php";
+    session_start();
 
-    function displayIndex(){
-        $productsConnection = getDatabaseConnection();
-        session_start();
-        $fetchedProducts = fetchProducts($productsConnection, "index");
-        if ($fetchedProducts->num_rows > 0) {
-            while($row = $fetchedProducts->fetch_assoc()) {
-                showProducts($row);
-
-                echo '</div >';
-                echo '<a href="?addToCart='.$row["id"].'">Add</a>';
-
-                echo '</div>';
-                echo '<br>';
-            }
+    $productsConnection = getDatabaseConnection();
+    $fetchedProducts = fetchProducts($productsConnection, "index");
+    $products = array();
+    if (isset($fetchedProducts) && $fetchedProducts->num_rows > 0) {
+        while($row = $fetchedProducts->fetch_assoc()) {
+           $products[] = $row;
         }
-        if(isset($_GET['addToCart'])){
-            addProduct($_GET['addToCart']);
-        }
-        echo '<a href="cart.php">Go to cart</a>';
     }
+    if(isset($_GET['addToCart'])){
+        addProduct(htmlspecialchars($_GET['addToCart']));
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,10 +26,22 @@
   </head>
   <body>
   <div class="products">
-  <?php
-    displayIndex();
-  die();
-  ?>
+      <?php foreach ($products as $product): ?>
+       <div class="product">
+          <img src="images/<?php echo htmlspecialchars($product['id']); ?>.png" alt="'.<?php echo htmlspecialchars($product['id']); ?>.'-image" height="100px" width="100px">
+          <div class="info">
+              <span class="title"><?php echo htmlspecialchars($product['title']); ?></span>
+              <br>
+              <span class="description"><?php echo htmlspecialchars($product['description']); ?></span>
+              <br>
+              <span class="price"><?php echo htmlspecialchars($product['price'].getCurrency());?></span>
+              <br>
+              </div >
+           <span><a href="?addToCart=<?php echo htmlspecialchars($product['id']); ?>">Add</a></span>
+       </div>
+      <br>
+      <?php endforeach ?>
+      <a href="cart.php">Go to cart</a>
   </div>
 
   </body>
