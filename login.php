eff4cdde723accf29b128bbc1b2451f8c88df317
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-10 16:16:35 +0300

    6 login.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/6
    added session parameters, logout and hashed password

--- login.php
+++ login.php
@@ -1,7 +1,41 @@
 <?php
-    require_once "commons.php";
-    $productsConnection = getDatabaseConnection();
 
+require_once 'common.php';
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(empty(trim($_POST['username']))){
+        $userError = 'Please enter username.';
+    } else{
+        $username = trim($_POST['username']);
+    }
+    if(empty(trim($_POST['password']))){
+        $passwordError = 'Please enter your password.';
+    } else{
+        $password = trim($_POST['password']);
+    }
+
+    $userLoginConnection = getDatabaseConnection();
+    $userLogin = $userLoginConnection->prepare('SELECT id,username,password from users where username=?');
+    $userLogin->bind_param('s',$username);
+    if($userLogin->execute()){
+        $users = $userLogin->get_result();
+        if($users->num_rows == 1){
+            while($user = $users->fetch_assoc()) {
+                if (isset($password) && password_verify($password, $user['password'])) {
+                    session_start();
+                    // Store data in session variables
+                    $_SESSION['loggedIn'] = true;
+                    $_SESSION['id'] = password_hash($user['id'], PASSWORD_BCRYPT);
+                    $_SESSION['username'] = $username;
+                    // Redirect user to products page
+                    header('location: products.php');
+                } else {
+                    $loginError = 'Invalid username or password';
+                }
+            }
+        }
+    }
+
+}
 
 ?>
 <!DOCTYPE html>
@@ -16,10 +50,21 @@
 <body>
 <div class="loginBody">
     <form class="login" method="post" action="login.php" >
-        Login
-        <input type="text" placeholder="Username" name="username" class="loginInputs">
-        <input type="text" placeholder="Password" name="password" class="loginInputs">
+        <h1>Login</h1>
+        <input type="text" placeholder="<?= translateText('Username')?>" name="username" class="loginInputs" value="">
+        <input type="text" placeholder="<?= translateText('Password')?>" name="password" class="loginInputs" value="">
         <input type="submit" class="loginInputs" value="Login">
+        <?php
+            if(isset($userError)){
+                echo translateText($userError);
+            }
+            if(isset($passwordError)){
+                echo translateText($passwordError);
+            }
+            if(isset($loginError)){
+                echo translateText($loginError);
+            }
+        ?>
     </form>
 </div>
 </body>
