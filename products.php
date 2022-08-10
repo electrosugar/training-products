1aff0bd299cf2adccf13c4475706cbe83b66f542
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-10 16:23:38 +0300

    7 products.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/7
    list all the products

--- products.php
+++ products.php
@@ -1,8 +1,12 @@
 <?php
-require_once "commons.php";
-$products_connection = getDatabaseConnection();
+
+require_once 'common.php';
+$products = getProducts('index');
 session_start();
-?>
+if(isset($_GET['action']) && $_GET['action'] == 'logout'){
+    logout();
+}
+;?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,38 +17,26 @@ session_start();
     <link rel="stylesheet" href="stylesheets/index.css">
 </head>
 <body>
+<h1>Welcome <?= $_SESSION['username'] ?> !</h1>
+<?php foreach ($products as $product): ?>
+    <div class="product">
+        <img src="images/<?= strip_tags($product['id']); ?>.png" alt="'.<?= strip_tags($product['id']); ?>.'-image" height="100px" width="100px">
+        <div class="info">
+            <span class="title"><?= strip_tags($product['title']); ?></span>
+            <br>
+            <span class="description"><?= strip_tags($product['description']); ?></span>
+            <br>
+            <span class="price"><?= strip_tags($product['price'].getCurrency());?></span>
+            <br>
+        </div >
+        <span><a href="product.php?editProduct=<?= strip_tags($product['id']); ?>">Edit   </a><a href="?deleteProduct=<?= strip_tags($product['id']); ?>">Delete</a></span>
+    </div>
+    <br>
+<?php endforeach ?>
+<a href="product.php">Add product</a>
+<a href="?action=logout">Logout</a>
 <?php
-$fetched_products = fetchProducts($products_connection, "all");
-    echo '<div class="products">';
-    if(!$fetched_products){
-        echo "No products in cart! <br>";
-    }
-    else{
-        if ($fetched_products->num_rows > 0) {
-            while($row = $fetched_products->fetch_assoc()) {
-                showProducts($row);
 
-                echo '</div >';
-                echo '<a href="?deleteProduct='.$row["id"].'">Delete</a>';
-                echo '</div>';
-                echo '<br>';
-            }
-        }
-    }
-    echo '<div class="products">';
-
-    if(isset($_GET['removeFromCart'])){
-        removeProduct($_GET['removeFromCart']);
-    }
-
-?>
-<form action="cart.php" method="post" class="form">
-    <?php echo translateText("Name") ?>: <input type="text" name="name" placeholder="Name"><br>
-    <?php echo translateText('Contact Details') ?> <input type="text" name="contact" placeholder="Contact Details"><br>
-    <?php echo translateText('Comments') ?> <input type="text" name="comments" placeholder="Comments" id="big"><br>
-    <span class="formLinks"> <input type="submit" name="Checkout"><a href="index.php">Go to index</a></span>
-</form>
-<?php
 die();
 ?>
 </body>
