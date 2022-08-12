64211412e110f0f9b24aa5edc2b9c997ac1546f4
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-12 18:16:10 +0300

    7 products.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/7
    added delete

--- products.php
+++ products.php
@@ -3,10 +3,26 @@
 require_once 'common.php';
 $products = getProducts('index');
 session_start();
+
+$pdoConnection = getDatabaseConnection();
+
 if(isset($_GET['action']) && $_GET['action'] == 'logout'){
     logout();
 }
-;?>
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    print_r($_POST);
+    if (isset($_POST)) {
+        $deleteProduct = $pdoConnection->prepare("DELETE FROM products WHERE id=? LIMIT 1");
+        if ($deleteProduct->execute([$_POST['delete']]) === TRUE) {
+            echo 'Record deleted successfully!';
+        } else {
+            echo 'Failed deleting record!';
+        }
+        header("Refresh:0");
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,7 +36,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'logout'){
 <h1>Welcome <?= $_SESSION['username'] ?> !</h1>
 <?php foreach ($products as $product): ?>
     <div class="product">
-        <img src="images/<?= strip_tags($product['id']); ?>.png" alt="'.<?= strip_tags($product['id']); ?>.'-image" height="100px" width="100px">
+        <img src="images/<?= strip_tags($product['id']); ?>.png" alt="<?= strip_tags($product['id']); ?>-image" height="100px" width="100px">
         <div class="info">
             <span class="title"><?= strip_tags($product['title']); ?></span>
             <br>
@@ -29,7 +45,12 @@ if(isset($_GET['action']) && $_GET['action'] == 'logout'){
             <span class="price"><?= strip_tags($product['price'].getCurrency());?></span>
             <br>
         </div >
-        <span><a href="product.php?editProduct=<?= strip_tags($product['id']); ?>">Edit   </a><a href="?deleteProduct=<?= strip_tags($product['id']); ?>">Delete</a></span>
+        <span>
+            <a href="product.php?productId=<?= strip_tags($product['id']); ?>"><?= translateText('Edit Items')?></a>
+            <form action="products.php" method="post">
+                <button type="submit" value="<?= strip_tags($product['id']); ?>" name='delete'><?= translateText('Delete')?></button>
+            </form>
+        </span>
     </div>
     <br>
 <?php endforeach ?>
