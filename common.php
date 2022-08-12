9cdb96cf23f6c363b116f60680fbd81734af2804
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-12 17:28:06 +0300

    8 common.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/8
    added function for product

--- common.php
+++ common.php
@@ -8,6 +8,7 @@ function getDatabaseConnection(){
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            //to prevent some edge case sql injections
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
         try {
@@ -27,37 +28,46 @@ function getCurrency(){
 //fetches specific products, where page specifies the select type for each page(all, cart or index)
 //cart = all cart products, index = all products not in the cart
 function fetchProducts($pdoConnection, $page){
-    $querryValues = '?';
+    $queryMarks = '?';
     if(isset($_SESSION['cart'])){
         if(count($_SESSION['cart']) === 0){
-            $querryValues = false;
+            $queryMarks = '';
         }elseif (count($_SESSION['cart']) > 1) {
-            $querryValues = $querryValues . str_repeat(',?', count($_SESSION['cart'])-1);
+            $queryMarks = $queryMarks . str_repeat(',?', count($_SESSION['cart'])-1);
         }
+    }else{
+        $queryMarks = '';
     }
 switch ($page){
     case 'cart':
-        if($querryValues){
-            $selectProducts = 'SELECT * from products where id in ('.$querryValues.')';
+        if($queryMarks){
+            $selectProducts = 'SELECT * from products where id in ('.$queryMarks.')';
         }
-        else  $selectProducts = 'SELECT * from products where id != id';
+        else {
+            $selectProducts = 'SELECT * from products where id != id';
+        }
         break;
     case 'index':
-        if($querryValues){
-            $selectProducts = 'SELECT * from products where not id in ('.$querryValues.')';
+        if($queryMarks){
+            $selectProducts = 'SELECT * from products where not id in ('.$queryMarks.')';
         }
         else{
             $selectProducts = 'SELECT * from products';
         }
         break;
-    case 'all': $selectProducts = 'SELECT * from products';
+    case 'all': {
+        $selectProducts = 'SELECT * from products';
+    }
         break;
-    default: $selectProducts = 'SELECT * from products';
+    default: {
+        $selectProducts = 'SELECT * from products';
+    }
 }
 
-if($querryValues){
+if($queryMarks){
     $statementSelectProducts = $pdoConnection->prepare($selectProducts);
-    $statementSelectProducts->execute([...$_SESSION['cart']]);
+    //the SESSION cart array can have indexes with no values that crash this function
+    $statementSelectProducts->execute(array_values($_SESSION['cart']));
 }
 else{
     $statementSelectProducts = $pdoConnection->query($selectProducts);
@@ -88,4 +98,17 @@ function logout(){
     session_unset();
     session_destroy();
     header('Location: login.php');
+}
+
+function addUpdateQueryColumns(& $updateValues, & $updateColumns, $columnName){
+    if(isset($_POST[$columnName]) && !empty($_POST[$columnName])){
+        $updateValues[] = $_POST[$columnName];
+        if($updateColumns){
+            $updateColumns .= ', '.$columnName.' = ?';
+        }
+        else {
+            $updateColumns = $columnName.' = ?';
+        }
+    }
+    return $updateColumns;
 }
