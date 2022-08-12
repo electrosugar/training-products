d7b874578acbe0ad561b01c29884fa520b201b2e
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-12 18:09:24 +0300

    8 common.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/
    added function for product

--- config.remote.php
+++ config.remote.php
@@ -4,3 +4,8 @@ define('SERVER_NAME','' );
 define('USER','' );
 define('PASSWORD', '' );
 define('DATABASE_NAME', '' );
+
+define('ADMIN_PASSWORD', '' );
+define('ADMIN_USERNAME', '' );
+
+define('SHOP_MANAGER_EMAIL', '');
