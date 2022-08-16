69fe3ea6559a9fe014a994b65bf076992eb42236
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-16 18:03:22 +0300

    5 cart.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/5
    email vendor sending

--- cart.php
+++ cart.php
@@ -16,22 +16,98 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 
-
     if (isset($_POST['name']) && isset($_POST['contact']) && isset($_POST['comment'])) {
         $name = strip_tags($_POST['name']);
         $contact = strip_tags($_POST['contact']);
         $comment = strip_tags($_POST['comment']);
 
-        $productsConnection = getDatabaseConnection();
+        $pdoConnection = getDatabaseConnection();
         if(!empty($name) && !empty($contact) && !empty($comment)){
-            $insertCustomers = $productsConnection->prepare('INSERT INTO customers (creation_date, name, contact, comment) VALUES (now(), ?, ?, ?)');
+            $insertCustomers = $pdoConnection->prepare('INSERT INTO customers (creation_date, name, contact, comment) VALUES (now(), ?, ?, ?)');
             $insertCustomers->execute([$name, $contact, $comment]);
-            $idCustomer = $productsConnection->lastInsertId();
+            $idCustomer = $pdoConnection->lastInsertId();
 
-            $insertOrder = $productsConnection->prepare('INSERT INTO orders (id_customer, id_product) VALUES ( ?, ?)');
+            $insertOrder = $pdoConnection->prepare('INSERT INTO orders (id_customer, id_product) VALUES ( ?, ?)');
             foreach($_SESSION['cart'] as $idProduct){
                 $insertOrder->execute([$idCustomer, $idProduct]);
             }
+
+            //send email
+            $selectCustomers = $pdoConnection->prepare('SELECT * FROM customers WHERE id = ?');
+            $selectCustomers->execute([$idCustomer]);
+            $customerInfo = $selectCustomers->fetch();
+            $mailTo = SHOP_MANAGER_EMAIL;
+            $mailSubject = 'Order from ' . $customerInfo['name'] . ' # ' . $customerInfo['id'];
+            $mailHeaders = "From: " . strip_tags(EMAIL_USERNAME) . PHP_EOL;
+            $mailHeaders .= "Reply-To: ". strip_tags(EMAIL_USERNAME) . PHP_EOL;
+            $mailHeaders .= 'MIME-Version: 1.0' . PHP_EOL;
+            $mailHeaders .= 'Content-Type: text/html; iso-8859-1' . PHP_EOL;
+
+            prepareOrderWithProducts($customerInfo, $customers);
+            $bound_text = '----*%$!$%*';
+            $bound = '--'.$bound_text.PHP_EOL;
+            $bound_last = '--'.$bound_text.'--'.PHP_EOL;
+
+            $mailSubject = 'Order from ' . $customerInfo['name'] . ' # ' . $customerInfo['id'];
+            $mailHeaders = "From: " . strip_tags(EMAIL_USERNAME) . PHP_EOL;
+            $mailHeaders .= "Reply-To: ". strip_tags(EMAIL_USERNAME) . PHP_EOL;
+            $mailHeaders .= 'MIME-Version: 1.0' . PHP_EOL;
+            $mailHeaders .= 'Content-Type: text/html; UTF-8' . PHP_EOL;
+
+            prepareOrderWithProducts($customerInfo, $customers);
+            //$mailMessage = 'Content-Type: text/html; charset=UTF-8'. PHP_EOL.'Content-Transfer-Encoding: 7bit'.PHP_EOL;
+            $mailMessage .= '<html><body>';
+
+
+            foreach ($customers as $customerDetail){
+                $mailMessage .= '<div class="order">';
+                $mailMessage .= '<div class="product">';
+                $mailMessage .= '<div class="info">';
+                $mailMessage .= '<span class="title">'. translateText('Name: ') . strip_tags($customerDetail['name']) .'</span>';
+                $mailMessage .= '<br>';
+                $mailMessage .= '<span class="description">' . translateText('Contact: ') . strip_tags($customerDetail['contact']). '</span>';
+                $mailMessage .= '<br>';
+                $mailMessage .= '<span class="price">' . translateText('Comment: ') . strip_tags($customerDetail['comment']). '</span>';
+                $mailMessage .= '<br>';
+                $mailMessage .= '<span class="date">' . translateText('Date: ') . strip_tags($customerDetail['creation_date']). '</span>';
+                $mailMessage .= '<br>';
+                $mailMessage .= '</div>';
+                $mailMessage .= '<span>' .translateText('Total Price: ') . $customerDetail['price'].getCurrency() . '</span>';
+                $mailMessage .= '</div>';
+                $mailMessage .= '<div class="selectedProducts">';
+                foreach ($customerDetail['productArray'] as $product){
+                    $mailMessage .= '<div class="product">';
+                    $productImage = file_get_contents('images/'.$product['id'].'.png');
+                    $productImageData = base64_encode($productImage);
+                    $mailMessage .= '<img src="cid:'.$product['id'].'.png">';
+                    $mailMessage .= 'Content-Type: image/png; name="'. $product['id'].'.png"'.PHP_EOL
+                        .'Content-Transfer-Encoding: base64'.PHP_EOL
+                        .'Content-ID: <'.$product['id'].'.png>'.PHP_EOL
+                        .PHP_EOL
+                        .chunk_split(base64_encode($productImage))
+                        .$bound_last;
+                    $mailMessage .= '<div class="info">';
+                    $mailMessage .= '<span class="title">' .strip_tags($product['title']). '</span>';
+                    $mailMessage .= '<br>';
+                    $mailMessage .= '<span class="description">' .strip_tags($product['description']). '</span>';
+                    $mailMessage .= '<br>';
+                    $mailMessage .= '<span class="price">' .strip_tags($product['price']). getCurrency(). '</span>';
+                    $mailMessage .= '<br>';
+                    $mailMessage .= '</div>';
+                    $mailMessage .= '</div>';
+                    $mailMessage .= '<br>';
+
+                }
+                $mailMessage .= '</div>';
+                $mailMessage .= '</div>';
+                $mailMessage .= '<br>';
+
+            }
+
+            $mailMessage .= '</body></html>';
+
+            mail($mailTo, $mailSubject, $mailMessage, $mailHeaders);
+
         }
         else{
             echo 'The order was not processed';
