7297189c8899621b3d78deb5104b1c0d16ba4f88
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-17 11:53:26 +0300

    8 product.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/8
    input values are kept and product adding needs all inputs to be filled, including the image file

--- product.php
+++ product.php
@@ -15,30 +15,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     addUpdateQueryColumns($updateValues, $updateMarks, 'price');
     $pdoConnection = getDatabaseConnection();
     if(isset($productEditId) && $productEditId != 0){
-        $editProduct = $pdoConnection->prepare('UPDATE products SET '.$updateMarks.' where id = ?');
-        $updateValues[] = $productEditId;
-        if($editProduct->execute($updateValues)){
-            echo 'Successful Edit!';
+        if($updateMarks){
+            $editProduct = $pdoConnection->prepare('UPDATE products SET '.$updateMarks.' where id = ?');
+            $updateValues[] = $productEditId;
+            if($editProduct->execute($updateValues)){
+                echo 'Successful Edit!';
+            }
         }
         $target_dir = 'images/';
         $target_file = $target_dir . $productEditId . '.png';
         if (move_uploaded_file($_FILES['image']['tmp_name'], $target_file)) {
-            echo 'The file '. htmlspecialchars( basename( $_FILES['image']['name'])). ' has been uploaded.';
+            echo 'The file '. strip_tags(basename( $_FILES['image']['name'])). ' has been uploaded.';
         }
     }
-    else if(count($updateValues)===3 && isset($_FILES['image']) && !empty($_FILES['image'])){
+    else if(count($updateValues)===3 && is_uploaded_file($_FILES ['image'] ['tmp_name'])){
         $addProduct = $pdoConnection->prepare('INSERT into products(title, description, price) VALUES (?, ?, ?)');
         if($addProduct->execute($updateValues)){
             echo 'Successful Product Insert!';
         }
         $target_dir = 'images/';
         $target_file = $target_dir . $pdoConnection->lastInsertId() . '.png';
-        if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-            echo 'The file '. htmlspecialchars( basename( $_FILES['image']['name'])). ' has been uploaded.';
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $target_file)) {
+            echo 'The file '. strip_tags(basename($_FILES['image']['name'])). ' has been uploaded.';
         }
     }
     else{
-        echo 'No product to edit, to add an item complete all fields';
+        echo 'To add an item complete all fields';
     }
 
 
@@ -58,9 +60,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <h1>Welcome <?= $_SESSION['username'] ?> !</h1>
 <form enctype="multipart/form-data" action="product.php" method="post" class="form">
     <input type="hidden" value="<?= strip_tags($_GET['productId']); ?>" name="id" />
-    <input type="text" name="title" placeholder="<?= translateText('Product Title') ?>"><br>
-    <input type="text" name="description" placeholder="<?= translateText('Description') ?>" id="big"><br>
-    <input type="text" name="price" placeholder="<?= translateText('Price') ?> "><br>
+    <input type="text" name="title" placeholder="<?= translateText('Product Title') ?>" value="<?= $value = isset($_POST['title'])?$_POST['title']:''; ?>"><br>
+    <input type="text" name="description" placeholder="<?= translateText('Description') ?>" value="<?= $value = isset($_POST['description'])?$_POST['description']:''; ?>" id="big"><br>
+    <input type="text" name="price" placeholder="<?= translateText('Price') ?> " value="<?= $value = isset($_POST['price'])?$_POST['price']:''; ?>"><br>
     <input type="file" name="image" value=<?= translateText('Browse') ?>><br>
     <span class="formLinks"> <input type="submit" value="Save"></span>
 </form>
