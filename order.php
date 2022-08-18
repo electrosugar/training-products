e661517c41164688006865533f14beef3287f318
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-18 13:42:59 +0300

    25 order.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/25
    appears after every cart order

--- order.php
+++ order.php
@@ -2,15 +2,19 @@
 
 require_once 'common.php';
 session_start();
-$databaseConnection = getDatabaseConnection();
-$selectAllCustomers = $databaseConnection->prepare('select * from customers');
-$selectAllCustomers->execute();
 
 $customers = [];
-$productArray = [];
-foreach($selectAllCustomers->fetchAll() as $row ) {
+$customer = [];
+if(isset($_GET['showOrder'])){
+    $databaseConnection = getDatabaseConnection();
+    $selectCustomer = $databaseConnection->prepare('select * from customers where id = ?');
+    $selectCustomer->execute([strip_tags($_GET['showOrder'])]);
+
+    $row = $selectCustomer->fetch();
     prepareOrderWithProducts($row, $customers);
+    $customer = $customers[0];
 }
+
 
 ?>
 <!DOCTYPE html>
@@ -23,40 +27,38 @@ foreach($selectAllCustomers->fetchAll() as $row ) {
     <link rel="stylesheet" href="stylesheets/index.css">
 </head>
 <body>
-<div class="products">
-    <?php foreach ($customers as $customerDetail): ?>
-        <div class="order">
-            <div class="product">
-                <div class="info">
-                    <span class="title"><?php echo translateText('Name: ') . strip_tags($customerDetail['name']); ?></span>
-                    <br>
-                    <span class="description"><?php echo translateText('Contact: ') . strip_tags($customerDetail['contact']); ?></span>
-                    <br>
-                    <span class="price"><?php echo translateText('Comment: ') . strip_tags($customerDetail['comment']);?></span>
-                    <br>
-                    <span class="date"><?php echo translateText('Date: ') . strip_tags($customerDetail['creation_date']);?></span>
-                    <br>
-                </div >
-                <span><?php echo translateText('Total Price: ') . $customerDetail['price'].getCurrency() ?></span>
-            </div>
-            <div class="selectedProducts">
-                <?php foreach ($customerDetail['productArray'] as $product): ?>
-                    <div class="product">
-                        <img src="images/<?= strip_tags($product['id']); ?>.png" alt="'.<?= strip_tags($product['id']); ?>.'-image" class="roundImage">
-                        <div class="info">
-                            <span class="title"><?= strip_tags($product['title']); ?></span>
-                            <br>
-                            <span class="description"><?= strip_tags($product['description']); ?></span>
-                            <br>
-                            <span class="price"><?= strip_tags($product['price'].getCurrency());?></span>
-                            <br>
-                        </div >
-                    </div>
-                <?php endforeach ?>
-            </div>
+<div class="orders">
+    <div class="order">
+        <h1>The order #<?= strip_tags($customer['id']); ?> has been recorded</h1>
+        <div class="product">
+            <div class="info">
+                <span class="title"><?= translateText('Name: ') . strip_tags($customer['name']); ?></span>
+                <br>
+                <span class="description"><?= translateText('Contact: ') . strip_tags($customer['contact']); ?></span>
+                <br>
+                <span class="price"><?= translateText('Comment: ') . strip_tags($customer['comment']);?></span>
+                <br>
+                <span class="date"><?= translateText('Date: ') . strip_tags($customer['creation_date']);?></span>
+                <br>
+            </div >
+            <span><?= translateText('Total Price: ') . $customer['price'].getCurrency() ?></span>
         </div>
-    <?php endforeach ?>
-
+        <div class="selectedProducts">
+            <?php foreach ($customer['productArray'] as $product): ?>
+                <div class="product">
+                    <img src="images/<?= strip_tags($product['id_product']); ?>.png" alt="'.<?= strip_tags($product['id_product']); ?>.'-image" class="roundImage">
+                    <div class="info">
+                        <span class="title"><?= strip_tags($product['title']); ?></span>
+                        <br>
+                        <span class="description"><?= strip_tags($product['description']); ?></span>
+                        <br>
+                        <span class="price"><?= strip_tags($product['price'].getCurrency());?></span>
+                        <br>
+                    </div >
+                </div>
+            <?php endforeach ?>
+        </div>
+    </div>
 </div>
 <a href="cart.php">Go to cart</a>
 <a href="cart.php">Go to index</a>
