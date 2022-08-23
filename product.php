021d1efa0134ba09fb1b1a1d3046aa0f0d5718e8
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-23 17:35:58 +0300

    8 product.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/8
    minor refactor

--- product.php
+++ product.php
@@ -1,50 +1,46 @@
 <?php
 
 require_once 'common.php';
-session_start();
-if((!isset($_SESSION['id']) || !isset($_SESSION['username']))){
+if ((!isset($_SESSION['id']) || !isset($_SESSION['username']))) {
     logout();
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $productEditId = $_POST['id'];
     $updateValues = [];
-    $updateMarks  = '';
+    $updateMarks = '';
     addUpdateQueryColumns($updateValues, $updateMarks, 'title');
     addUpdateQueryColumns($updateValues, $updateMarks, 'description');
     addUpdateQueryColumns($updateValues, $updateMarks, 'price');
-    $pdoConnection = getDatabaseConnection();
-    if(isset($productEditId) && $productEditId != 0){
-        if($updateMarks){
-            $editProduct = $pdoConnection->prepare('UPDATE products SET '.$updateMarks.' where id = ?');
+    if (isset($productEditId) && $productEditId != 0) {
+        if ($updateMarks) {
+            $editProduct = $pdoConnection->prepare('UPDATE products SET ' . $updateMarks . ' where id = ?');
             $updateValues[] = $productEditId;
-            if($editProduct->execute($updateValues)){
-                echo 'Successful Edit!';
+            if ($editProduct->execute($updateValues)) {
+                $success = 'Successful Edit!';
             }
         }
         $target_dir = 'images/';
         $target_file = $target_dir . $productEditId . '.png';
         if (move_uploaded_file($_FILES['image']['tmp_name'], $target_file)) {
-            echo 'The file '. strip_tags(basename( $_FILES['image']['name'])). ' has been uploaded.';
+            $successFileUpload = 'The file ' . strip_tags(basename($_FILES['image']['name'])) . ' has been uploaded.';
         }
-    }
-    else if(count($updateValues)===3 && is_uploaded_file($_FILES ['image'] ['tmp_name'])){
+    } else if (count($updateValues) === 3 && is_uploaded_file($_FILES ['image'] ['tmp_name'])) {
         $target_dir = 'images/';
         $addProduct = $pdoConnection->prepare('INSERT into products(title, description, price) VALUES (?, ?, ?)');
         $addOldProduct = $pdoConnection->prepare('INSERT into old_products(title, description, price) VALUES (?, ?, ?)');
-        if($addProduct->execute($updateValues)){
-            echo 'Successful Product Insert!';
+        if ($addProduct->execute($updateValues)) {
+            $success = 'Successful Product Insert!';
         }
         $target_file = $target_dir . $pdoConnection->lastInsertId() . '.png';
         $addOldProduct->execute($updateValues);
         $old_file = $target_dir . $pdoConnection->lastInsertId() . 'OLD.png';
         if (move_uploaded_file($_FILES['image']['tmp_name'], $target_file)) {
-            echo 'The file '. strip_tags(basename($_FILES['image']['name'])). ' has been uploaded.';
+            $successFileUpload = 'The file ' . strip_tags(basename($_FILES['image']['name'])) . ' has been uploaded.';
         }
         copy($target_file, $old_file);
-    }
-    else{
-        echo 'To add an item complete all fields';
+    } else {
+        $failure = 'To add an item complete all fields';
     }
 
 
@@ -57,20 +53,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add or Edit Product</title>
+    <title><?= translateText('Add or edit product') ?></title>
     <link rel="stylesheet" href="stylesheets/index.css">
 </head>
 <body>
 <h1>Welcome <?= $_SESSION['username'] ?> !</h1>
 <form enctype="multipart/form-data" action="product.php" method="post" class="form">
-    <input type="hidden" value="<?= strip_tags($_GET['productId']); ?>" name="id" />
-    <input type="text" name="title" placeholder="<?= translateText('Product Title') ?>" value="<?= $value = isset($_POST['title'])?$_POST['title']:''; ?>"><br>
-    <input type="text" name="description" placeholder="<?= translateText('Description') ?>" value="<?= $value = isset($_POST['description'])?$_POST['description']:''; ?>" id="big"><br>
-    <input type="text" name="price" placeholder="<?= translateText('Price') ?> " value="<?= $value = isset($_POST['price'])?$_POST['price']:''; ?>"><br>
+    <input type="hidden" value="<?= strip_tags($_GET['productId']); ?>" name="id"/>
+    <input type="text" name="title" placeholder="<?= translateText('Product Title') ?>"
+           value="<?= $value = isset($_POST['title']) ? $_POST['title'] : ''; ?>"><br>
+    <input type="text" name="description" placeholder="<?= translateText('Description') ?>"
+           value="<?= $value = isset($_POST['description']) ? $_POST['description'] : ''; ?>" id="big"><br>
+    <input type="text" name="price" placeholder="<?= translateText('Price') ?> "
+           value="<?= $value = isset($_POST['price']) ? $_POST['price'] : ''; ?>"><br>
     <input type="file" name="image" value=<?= translateText('Browse') ?>><br>
     <span class="formLinks"> <input type="submit" value="Save"></span>
 </form>
-<a href="products.php"><?= translateText('Products')?></a>
+<a href="products.php"><?= translateText('Products') ?></a>
+<?= isset($success) ? translateText($success) : '' ?>
+<?= isset($successFileUpload) ? translateText($successFileUpload) : '' ?>
+<?= isset($failure) ? translateText($failure) : '' ?>
 <?php
 die();
 ?>
