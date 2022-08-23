4f5ca7136e5d0fa00cc84713dd8f45016a694f4e
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-23 17:36:31 +0300

    7 products.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/7
    minor refactor

--- products.php
+++ products.php
@@ -1,24 +1,22 @@
 <?php
 
 require_once 'common.php';
-$products = getProducts('index');
-session_start();
+$selectProducts = 'SELECT * from products';
+$products = getProductsArray('', $pdoConnection, $selectProducts);
 
-$pdoConnection = getDatabaseConnection();
 
-if(isset($_GET['action']) && $_GET['action'] == 'logout' || (!isset($_SESSION['id']) || !isset($_SESSION['username']))){
+if (isset($_GET['action']) && $_GET['action'] == 'logout' || (!isset($_SESSION['id']) || !isset($_SESSION['username']))) {
     logout();
 }
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    print_r($_POST);
     if (isset($_POST)) {
         $deleteProduct = $pdoConnection->prepare('DELETE FROM products WHERE id=? LIMIT 1');
         if ($deleteProduct->execute([$_POST['delete']]) === TRUE) {
-            echo 'Record deleted successfully!';
+            $success = 'Record deleted successfully!';
         } else {
-            echo 'Failed deleting record!';
+            $failure = 'Failed deleting record!';
         }
-        header('Refresh:0');
+        header('Location: products.php');
     }
 }
 
@@ -29,26 +27,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Products</title>
+    <title><?= translateText('Products') ?></title>
     <link rel="stylesheet" href="stylesheets/index.css">
 </head>
 <body>
+<?= isset($success) ? translateText($success) : '' ?>
+<?= isset($failure) ? translateText($failure) : '' ?>
 <h1>Welcome <?= $_SESSION['username'] ?> !</h1>
 <?php foreach ($products as $product): ?>
     <div class="product">
-        <img src="images/<?= strip_tags($product['id']); ?>.png" alt="<?= strip_tags($product['id']); ?>-image" height="100px" width="100px">
+        <img src="images/<?= strip_tags($product['id']); ?>.png" alt="<?= strip_tags($product['id']); ?>-image"
+             height="100px" width="100px">
         <div class="info">
             <span class="title"><?= strip_tags($product['title']); ?></span>
             <br>
             <span class="description"><?= strip_tags($product['description']); ?></span>
             <br>
-            <span class="price"><?= strip_tags($product['price'].getCurrency());?></span>
+            <span class="price"><?= strip_tags($product['price'] . getCurrency()); ?></span>
             <br>
-        </div >
+        </div>
         <span>
-            <a href="product.php?productId=<?= strip_tags($product['id']); ?>"><?= translateText('Edit Items')?></a>
+            <a href="product.php?productId=<?= strip_tags($product['id']); ?>"><?= translateText('Edit Items') ?></a>
             <form action="products.php" method="post">
-                <button type="submit" value="<?= strip_tags($product['id']); ?>" name='delete'><?= translateText('Delete')?></button>
+                <button type="submit" value="<?= strip_tags($product['id']); ?>"
+                        name='delete'><?= translateText('Delete') ?></button>
             </form>
         </span>
     </div>
