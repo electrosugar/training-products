cecf117dd10d70048e044fd4682e0ae8286f0f28
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-23 17:31:39 +0300

    8 common.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/8
    code style

--- config.remote.php
+++ config.remote.php
@@ -1,11 +1,11 @@
 <?php
 
-define('SERVER_NAME','' );
-define('USER','' );
-define('PASSWORD', '' );
-define('DATABASE_NAME', '' );
+define('SERVER_NAME', '');
+define('USER', '');
+define('PASSWORD', '');
+define('DATABASE_NAME', '');
 
-define('ADMIN_PASSWORD', '' );
-define('ADMIN_USERNAME', '' );
+define('ADMIN_PASSWORD', '');
+define('ADMIN_USERNAME', '');
 
 define('SHOP_MANAGER_EMAIL', '');
