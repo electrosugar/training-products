4110a9a1a1ab7e581831b6cf7a24c7f7f4af6a77
electrosugar <razvan.mocanu@5psolutions.eu>
2022-08-31 16:48:58 +0300

    9 orders.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/9 changed db model for orders

--- orders.php
+++ orders.php
@@ -3,13 +3,35 @@
 require_once 'common.php';
 
 $pdoConnection = getDatabaseConnection();
-$selectAllCustomers = $pdoConnection->prepare('select * from customers');
-$selectAllCustomers->execute();
+$selectAllOrders = $pdoConnection->prepare('SELECT * FROM orders');
+//$selectAllOrders = $pdoConnection->prepare('SELECT OP.id_order,
+//                                                               O.comment,
+//                                                               O.contact,
+//                                                               O.name,
+//                                                               GROUP_CONCAT(P.title SEPARATOR \',\') AS titles,
+//                                                               GROUP_CONCAT(P.description SEPARATOR \',\') AS descriptions,
+//                                                               GROUP_CONCAT(OP.price SEPARATOR \',\') AS prices,
+//                                                               GROUP_CONCAT(OP.quantity SEPARATOR \',\') AS quantities,
+//                                                               GROUP_CONCAT(OP.id_product SEPARATOR \',\') AS product_ids FROM products P
+//                                                                   INNER JOIN order_product OP ON OP.id_product = P.id  INNER JOIN orders O ON O.id = OP.id_order GROUP BY OP.id_order');
+$selectAllOrders->execute();
 
-$customers = [];
+$orders = [];
 $productArray = [];
-foreach ($selectAllCustomers->fetchAll() as $row) {
-    prepareOrderWithProducts($row, $customers);
+
+foreach ($selectAllOrders->fetchAll() as $row) {
+//    $orders['name'] = $row['name'];
+//    $orders['comment'] = $row['comment'];
+//    $orders['contact'] = $row['contact'];
+//    $prices = explode( ',', $row['titles']);
+//    $descriptions = explode( ',', $row['descriptions']);
+//    $prices = explode( ',', $row['prices']);
+//    $quantities = explode( ',', $row['quantities']);
+//    $productId = explode( ',', $row['product_ids']);
+//    print_r($prices);
+    echo '<br>';
+
+    prepareOrderWithProducts($row, $orders);
 }
 
 ?>
@@ -24,34 +46,38 @@ foreach ($selectAllCustomers->fetchAll() as $row) {
 </head>
 <body>
 <div class="products">
-    <?php foreach ($customers as $customerDetail): ?>
+    <?php foreach ($orders as $order): ?>
         <div class="order">
+            <h2><?= translateText('Order #') . $order['id'] ?> </h2>
             <div class="product">
                 <div class="info">
-                    <span class="title"><?= translateText('Name: ') . $customerDetail['name'] ?></span>
+                    <span class="title"><?= translateText('Name: ') . $order['name'] ?></span>
                     <br>
-                    <span class="description"><?= translateText('Contact: ') . $customerDetail['contact'] ?></span>
+                    <span class="description"><?= translateText('Contact: ') . $order['contact'] ?></span>
                     <br>
-                    <span class="price"><?= translateText('Comment: ') . $customerDetail['comment'] ?></span>
+                    <span class="price"><?= translateText('Comment: ') . $order['comment'] ?></span>
                     <br>
-                    <span class="date"><?= translateText('Date: ') . $customerDetail['creation_date'] ?></span>
+                    <span class="date"><?= translateText('Date: ') . $order['creation_date'] ?></span>
                     <br>
                 </div>
-                <span><?= translateText('Total Price: ') . $customerDetail['price'] . getCurrency() ?></span>
+                <span><?= translateText('Total Price: ') . $order['price'] . getCurrency() ?></span>
             </div>
             <div class="selectedProducts">
-                <?php foreach ($customerDetail['productArray'] as $product): ?>
+                <?php foreach ($order['productArray'] as $product): ?>
                     <div class="product">
-                        <img src="images/<?= $product['id'] ?>OLD.png"
-                             alt="<?= $product['id'] ?>-image" class="roundImage">
+                        <img src="images/<?= $value = isset($product['id']) ? $product['id'] : translateText('missing id ') ?>.png"
+                             alt="<?= $value = isset($product['id']) ? $product['id'] : translateText('missing id ') ?>-image"
+                             class="roundImage">
                         <div class="info">
-                            <span class="title"><?=translateText('Title: ') . $product['title'] ?></span>
+                            <span class="title"><?= translateText('Title: ') . $value = isset($product['title']) ? $product['title'] : '<span class="error">' . translateText('missing title ') . '</span>' ?></span>
                             <br>
-                            <span class="description"><?=translateText('Description: ') . $product['description'] ?></span>
+                            <span class="description"><?= translateText('Description: ') . $value = isset($product['description']) ? $product['description'] : '<span class="error">' . translateText('missing description ') . '</span>' ?></span>
                             <br>
-                            <span class="price"><?=translateText('Price: ') . $product['price'] . getCurrency() ?></span>
+                            <span class="price"><?= translateText('Price per item: ') . $product['price'] . getCurrency() ?></span>
                             <br>
                             <span class="quantity"><?= translateText('Quantity: ') . $product['quantity'] ?></span>
+                            <br>
+                            <span class="quantity"><?= translateText('Price: ') . $product['price'] * $product['quantity'] . getCurrency() ?></span>
                             <br>
                         </div>
                     </div>
