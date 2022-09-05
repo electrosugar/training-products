62dbd5bfedb86ac8062e07688120d9fd423f92e6
electrosugar <razvan.mocanu@5psolutions.eu>
2022-09-05 17:57:33 +0300

    25 orders.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-mocanu/tasks/25 added layout

--- orders.php
+++ orders.php
@@ -9,12 +9,7 @@ $selectAllOrders = $pdoConnection->prepare('SELECT   OP.id_order,
                                                                O.contact,
                                                                O.comment,
                                                                O.creation_date,
-                                                               GROUP_CONCAT(COALESCE(P.title, \'NULL\') SEPARATOR \',\') AS titles,
-                                                               GROUP_CONCAT(COALESCE(P.description, \'NULL\') SEPARATOR \',\') AS descriptions,
-                                                               GROUP_CONCAT(OP.price SEPARATOR \',\') AS prices,
-                                                               GROUP_CONCAT(OP.quantity SEPARATOR \',\') AS quantities,
-                                                               SUM(OP.quantity * OP.price) AS totalPrice,
-                                                               GROUP_CONCAT(COALESCE(OP.id_product, \'NULL\') SEPARATOR \',\') AS product_ids FROM products P
+                                                               SUM(OP.quantity * OP.price) AS totalPrice FROM products P
                                                                RIGHT OUTER JOIN order_product OP ON OP.id_product = P.id  INNER JOIN orders O ON O.id = OP.id_order GROUP BY OP.id_order');
 $selectAllOrders->execute();
 
@@ -37,27 +32,14 @@ foreach ($selectAllOrders->fetchAll() as $row) {
 <div class="products">
     <?php foreach ($orders as $order): ?>
         <div class="order">
-            <h2><?= translateText('Order #') . $order['id'] ?> </h2>
-            <div class="product">
-                <div class="info">
-                    <span class="title"><?= translateText('Name: ') . $order['name'] ?></span>
-                    <br>
-                    <span class="description"><?= translateText('Contact: ') . $order['contact'] ?></span>
-                    <br>
-                    <span class="price"><?= translateText('Comment: ') . $order['comment'] ?></span>
-                    <br>
-                    <span class="date"><?= translateText('Date: ') . $order['creation_date'] ?></span>
-                    <br>
-                </div>
-                <span><?= translateText('Total Price: ') . $order['totalPrice'] . getCurrency() ?></span>
-            </div>
+            <?php require 'layouts/order.layout.php'; ?>
             <a href="order.php?orderId=<?= $order['id'] ?>"><?= translateText('View Order Details') ?></a>
         </div>
     <?php endforeach ?>
-
+</body>
 </div>
-<a href="cart.php"><?= translateText('Go to cart') ?></a>
-<a href="index.php"><?= translateText('Go to index') ?></a>
+<a href="products.php"><?= translateText('Go to products') ?></a>
+<a href="product.php"><?= translateText('Go to product') ?></a>
 </body>
 <?php
 
